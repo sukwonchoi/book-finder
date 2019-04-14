c63c25bc3e5a55099efcd221716d7f80128246aa
Kevin Choi <kevinc@olympic.co.nz>
2019-04-14 17:59:43 +1200

    can enter search query and displays a list of books on the screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+interface Book {
+  id: string;
+  volumeInfo: { title: string }
+}
+
+type LoadingState = 'initial' | 'loading' | 'success' | 'failure';
+
+type State = typeof initialState;
+const initialState = {
+  books: [] as ReadonlyArray<Book>,
+  loadingState: 'initial' as LoadingState,
+  searchTerm: '' as string,
+}
+
+
+class App extends Component<{}, State> {
+  state = initialState;
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Book Finder</h1>
+        <input value={this.state.searchTerm} onChange={this._onSearchInputChange} type="text"/>
+        <button onClick={this._onSearch}>Search</button>
+        <ul>
+          {this.state.books.map(book => (
+            <li key={book.id}>{book.volumeInfo.title}</li>
+          ))}
+        </ul>
       </div>
     );
+  }
+
+  _onSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({searchTerm: event.target.value})
+  }
+
+  _onSearch = () => {
+    const searchTerm = this.state.searchTerm;
+    if (searchTerm) {
+      this._getBooks(searchTerm);
+    }
+  }
+
+  _getBooks = async (searchTerm: string) => {
+    let url = `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`;
+    const res = await fetch(url);
+    const res_1 = await res.json();
+    this.setState({ books: res_1.items });
   }
 }
 
